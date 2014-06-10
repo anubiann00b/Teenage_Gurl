1fa6898b8e3a255b94a348247d97f26021b7d91e
Stefan <stefan@stefanaleksic.com>
2014-06-09 22:29:36 -0500

    added yasha and gabes ideas

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from random import randrange
 
-possibleWords = ['like idk', 'omg shut up you are so annoying','just shut up','wat r u tlking about?!','omg','idk','lol']
+possibleWords = ['like idk', 'omg shut up you are so annoying','just shut up','wat r u tlking about?!','omg','idk','lol', "I can't even", "I'm so done!"]
 
 while(1):
 	rInput = raw_input("You >> ")
