185fd9b0a48f5cb90a493fe399e3c91e6b47a249
Stefan Aleksic <soccerfanatic1996@gmail.com>
2014-06-09 22:31:30 -0500

    removed an idiotic variable assignment

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from random import randrange
 possibleWords = ['like idk', 'omg shut up you are so annoying','just shut up','wat r u tlking about?!','omg','idk','lol', "I can't even", "I'm so done!"]
 
 while(1):
-	rInput = raw_input("You >> ")
+	raw_input("You >> ")
 	print "Katy t3h PeNgU1N oF d00m >> " + possibleWords[randrange(len(possibleWords))]
